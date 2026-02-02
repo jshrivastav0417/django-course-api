daaaa3657994de36938c4d20188b4dc70ecc0d5a
jshrivastav0417 <jshrivastav22@gmail.com>
2026-02-02 21:21:54 +0530

    Remove learning urls include

--- core/core/urls.py
+++ core/core/urls.py
@@ -7,7 +7,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("learning.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("silk/", include("silk.urls", namespace="silk")),
 ]
