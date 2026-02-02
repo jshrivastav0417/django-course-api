28686a2f0af409914401f059179a9b38e06b75e2
jshrivastav0417 <jshrivastav22@gmail.com>
2026-02-02 09:56:16 +0530

    Initial Django course project

--- core/core/settings.py
+++ core/core/settings.py
@@ -4,6 +4,7 @@ Django settings for core project.
 
 from pathlib import Path
 import os
+import dj_database_url 
 
 # Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
